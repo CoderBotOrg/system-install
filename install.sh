4ef3b03346416455f406b8bfef32f9e1a1045e87
Roberto <roberto.previtera@gmail.com>
2018-12-05 23:59:42 +0100

    added hosts; fix ip range

--- install.sh
+++ install.sh
@@ -4,11 +4,14 @@ apt-get install -y hostapd dnsmasq
 
 mkdir /etc/coderbot
 
+cp etc/hostname /etc/.
+cp etc/hosts /etc/.
 cp etc/init.d/* /etc/init.d/.
 cp etc/hostapd/* /etc/hostapd/.
+cp etc/dnsmasq.conf /etc/.
 cp etc/coderbot/* /etc/coderbot/.
-cp etc/hostname /etc/.
 
 systemctl enable coderbot
+systemctl disable hostapd
 systemctl enable wifi
 
