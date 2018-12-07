b45a94de19db153a715eb37919d067aa8a5e704d
Roberto <roberto.previtera@gmail.com>
2018-12-07 15:37:09 +0100

    fix some incorrect su-sudo

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 apt-get install -y hostapd dnsmasq pigpio espeak gpac portaudio19-dev \
-	           git python3-pip python3 python3-venv libopenjp2-7-dev \
-		   libtiff5 libatlas-base-dev libhdf5-dev \
-		   libhdf5-serial-dev python-gobject libharfbuzz-bin \
-		   libwebp6 libjasper1 libilmbase12 libgstreamer1.0-0 \
-		   libavcodec-extra57 libavformat57 libopencv-dev \
-		   libqtgui4 libqt4-test omxplayer libhdf5-dev
+                   git python3-pip python3 python3-venv libopenjp2-7-dev \
+                   libtiff5 libatlas-base-dev libhdf5-dev \
+                   libhdf5-serial-dev python-gobject libharfbuzz-bin \
+                   libwebp6 libjasper1 libilmbase12 libgstreamer1.0-0 \
+                   libavcodec-extra57 libavformat57 libopencv-dev \
+                   libqtgui4 libqt4-test omxplayer libhdf5-dev
 
-mkdir /etc/coderbot
+mkdir -p /etc/coderbot
 
 cp etc/hostname /etc/.
 cp etc/hosts /etc/.
@@ -18,15 +18,16 @@ cp etc/dnsmasq.conf /etc/.
 cp etc/coderbot/* /etc/coderbot/.
 cp etc/modprobe.d/alsa-base.conf /etc/modprobe.d/.
 
-su pi
+sudo -u pi bash << EOF
 ./download_mobilenet_v1_models.sh
 cd /home/pi
 wget https://github.com/CoderBotOrg/backend/archive/develop.zip
-unzip backend-develop.zip
+unzip develop.zip
+rm develop.zip
 mv backend-develop coderbot
-cd coderbot
+EOF
 
-exit
+cd ../coderbot
 pip3 install -r requirements_stub.txt
 pip3 install -r requirements.txt
 
@@ -34,9 +35,8 @@ cd ..
 
 systemctl disable hostapd
 systemctl enable coderbot
-systemctl enable pigpio
+systemctl enable pigpiod
 systemctl enable wifi
-systemctl start pigpio
+systemctl start pigpiod
 systemctl start wifi
 systemctl start coderbot
-
