01181abdc2a0a5e5815c30e78f19e7dc5315c952
sorre97 <sorre97@hotmail.it>
2019-02-19 11:45:36 +0100

    Removed sleep between messages

--- etc/scripts/reset_trigger.py
+++ etc/scripts/reset_trigger.py
@@ -12,14 +12,9 @@ pi.set_pull_up_down(BUTTON_PIN, pigpio.PUD_UP)
 
 while(True):
         if(pi.wait_for_edge(BUTTON_PIN, edge = pigpio.FALLING_EDGE,  wait_timeout = 5)):
-                print("***DO NOT TURN OFF THE SYSTEM during the process\n" +
-			"or you will damage the system irreversibly***")
-		time.sleep(5)
-		print("Backing up boot partition")
-                time.sleep(2)
+                print("Backing up boot partition")
                 #subprocess.call(["sudo", "backup_boot"])
                 print("Boot partition back up finished")
-                time.sleep(2)
                 print("Restoring boot partition")
                 #subprocess.call(["sudo", "/home/pi/update-reset/src/sbin/restore_boot"])
                 print("Boot restore finished")
