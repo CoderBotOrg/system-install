3ae746d8ea9e6193386532c0e7506eaab4e1de6e
sorre97 <sorre97@hotmail.it>
2019-02-25 15:55:22 +0100

    Reset script

--- etc/scripts/reset_trigger.py
+++ etc/scripts/reset_trigger.py
@@ -1,35 +1,31 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import pigpio
-import subprocess
 import time
 import os
 
+# setting up service script parameters
 BUTTON_PIN = 16
 
 pi = pigpio.pi('localhost')
 pi.set_mode(BUTTON_PIN, pigpio.INPUT)
 pi.set_pull_up_down(BUTTON_PIN, pigpio.PUD_UP)
 
-doreset = False #flag to see if restore is on
-doreset1 = os.path.isfile('/boot/doreset')      # checks if flag doreset is there
-#doreset2 = SECOND/PATH/TO/FLAG
 
 
 while(True):
-        # prints in log the tail of the restore process (after reboot and restore)
-        if(doreset and (not doreset1)):
-                print("[ OK ]")
-                print("Restoring boot kernel...")
-                print("[ OK ]") 
-                doreset = False
-
-        if(pi.wait_for_edge(BUTTON_PIN, edge = pigpio.FALLING_EDGE,  wait_timeout = 5)):
-                doreset = True
-                #deletes the log file. if that doesn't exist, create one
-                with open('/home/pi/log/reset_trigger_service.log', 'w+'): pass
-                print("Creating FLAG (doreset) for reset...")
-                open("/home/pi/doreset", 'a').close()   # creates flag doreset inside (/boot)
-                print("[ OK ]")
-                print("Rebooting...")
-                os.system('sudo reboot')      			# reboots system to trigger reset
+		#waiting for falling edge
+		if(pi.wait_for_edge(BUTTON_PIN, edge = pigpio.FALLING_EDGE)):
+				start_time = time.time() 						   # initial time
+				while pi.read(BUTTON_PIN) == 0: 				   # counts how much time button is pressed
+						buttonTime = time.time() - start_time	  # How long was the button down?
+						if buttonTime >= 3:
+								#deletes the log file. if that doesn't exist, create one
+								with open('/home/pi/log/reset_trigger_service.log', 'w+'): pass
+								print("Creating FLAG (doreset) for reset...")
+								open("/boot/doreset", 'a').close() # creates flag doreset inside (/boot)
+								print("[ OK ]")
+								print("Rebooting + boot kernel restore...")
+								os.system('sudo reboot')		   # reboots system to trigger reset
+								break 							   # avoid duplicate in logs because of signal delay
