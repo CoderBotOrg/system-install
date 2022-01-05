45b581adba0a540f4e7e00b9842ca0c577b1a26a
previ <roberto.previtera@gmail.com>
2022-01-05 11:13:23 +0000

    add chmod firmware/upload.sh

--- install_firmware.sh
+++ install_firmware.sh
@@ -2,3 +2,4 @@
 mkdir -p /home/pi/coderbot/firmware
 wget -nc https://raw.githubusercontent.com/CoderBotOrg/firmware/master/bin/upload.sh -O /home/pi/coderbot/firmware/upload.sh
 wget -nc https://raw.githubusercontent.com/CoderBotOrg/firmware/master/dist/atmega.hex -O /home/pi/coderbot/firmware/atmega.hex
+chmod +x /home/pi/coderbot/firmware/upload.sh
